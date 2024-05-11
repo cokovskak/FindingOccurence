ee235ce384c09aee8fc34ad9553bcf7f1134f4cc
cokovskak <cokovskak@gmail.com>
2024-05-11 23:52:53 +0200

    Proekt kriptografija v1
    Signed-off-by: cokovskak <cokovskak@gmail.com>

--- src/FindingLFSR.java
+++ src/FindingLFSR.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.Scanner;
 
 public class FindingLFSR {
     static int[] poly1 = {1,0,0,0,0,0,1}; // feedback polynomial of degree 7
@@ -25,18 +26,18 @@ public class FindingLFSR {
     }
     public static void main(String[] args)
     {
-        BufferedReader ifp=null;
-        BufferedWriter ofp=null;
+       BufferedReader ifp=null;
+        PrintWriter ofp=null;
         try {
-           ifp = new BufferedReader(new FileReader("/home/kristina/Desktop/FINKI/Semestar6/K/FindingOccurence/key_seq.txt"));
-           ofp = new BufferedWriter(new FileWriter("result.txt"));
+            ifp = new BufferedReader(new FileReader("/home/kristina/Desktop/FINKI/Semestar6/K/FindingOccurence/key_seq.txt"));
+            ofp = new PrintWriter(new FileWriter("result.txt"));
 
             int[] init_st1, init_st2, init_st3;
             int[] deriv_st1, deriv_st2, deriv_st3;
-            int i, j, k = 0, t, x1, x2, x3, f;
+            int i, k = 0, x1, x2, x3, f;
             int cnt1 = 0, cnt2 = 0, cnt3 = 0;
             char ch;
-          //  int st, st1, st3;
+            int st, st1, st2;
             int[] z;
             init_st1 = new int[l1];
             deriv_st1 = new int[l1];
@@ -49,20 +50,57 @@ public class FindingLFSR {
             init_st3 = new int[l3];
             deriv_st3 = new int[l3];
             arr3 = new int[l3];
-//Read the bits from the previosly generated keystream and store it in z[]
+            //Read the bits from the previosly generated keystream and store it in z[]
             z = new int[cycles];
             for (i = 0; i < cycles; i++) {
                 ch = (char) ifp.read();
                 z[i] = ch - 48;
             }
 
-//Find the initial state of LFSR-1
-            ofp.write("Initial value of LFSR-1:");
+            //Find the initial state of LFSR-1
+            ofp.write("Initial value of LFSR-1: ");
             findInitialSequence(ofp, init_st1, deriv_st1, z, l1, arr1, poly1);
 
-            // find the initial state of lfsr-3
-            ofp.write("Initial value of LFSR-3:");
+            // find the initial state of LFSR-3
+            ofp.write("Initial value of LFSR-3: ");
             findInitialSequence(ofp, init_st3, deriv_st3, z, l3, arr3, poly3);
+
+            //Find the initial state of lfsr-2 with exhaustive research
+            for(st2=1;st2<Math.pow(2, l2);st2++)
+            {
+                for(i=0;i<l2;i++)
+                {
+                    arr2[i]=init_st2[i]=(st2>>i)&0x1;
+                }
+                for(i=l1-1;i>=0;i--)
+                    arr1[i]=deriv_st1[i];
+
+                for(i=l2-1;i>=0;i--)
+                    arr2[i]=init_st2[i];
+
+                for(i=l3-1;i>=0;i--)
+                    arr3[i]=deriv_st3[i];
+                for(k=0;k<cycles;k++)
+                {
+                    x1=LFSR(l1,arr1,poly1);
+                    x2=LFSR(l2,arr2,poly2);
+                    x3=LFSR(l3,arr3,poly3);
+                    f=(x1*x2)^(x2*x3)^x3;
+                    if(f!=z[k])
+                        break;
+
+                }
+                if(k==cycles)
+                {
+                    ofp.write("Found\n");
+                    ofp.write("Initial state for LFSR-2: ");
+                    for(i=l2-1;i>=0;i--)
+                    {
+                        deriv_st2[i]=init_st2[i];
+                       ofp.write(String.format("%d\t",init_st2[i]));
+                    }
+                }
+            }
         }catch (IOException e) {
             e.printStackTrace();
         } finally {
@@ -77,36 +115,35 @@ public class FindingLFSR {
         }
     }
 
-    private static void findInitialSequence(BufferedWriter ofp, int[] init_st3, int[] deriv_st3, int[] z, int l3, int[] arr3, int[] poly3) throws IOException {
+    private static void findInitialSequence(PrintWriter ofp, int[] init_st, int[] deriv_st, int[] z, int l, int[] arr, int[] poly) throws IOException {
         int st;
         int i;
-        int cnt3;
+        int cnt;
         int t;
-        int x3;
-        for(st=0; st<Math.pow(2, l3); st++)
+        int x;
+        for(st=0; st<Math.pow(2, l); st++)
         {
-            for(i=0; i< l3; i++)
+            for(i=0; i< l; i++)
             {
-                arr3[i]=init_st3[i]=(st>>i)&0x1;
-               // System.out.printf(init_st3[i]+"\t");
+                arr[i]=init_st[i]=(st>>i)&0x1;
+
             }
-            cnt3=0;
+            cnt=0;
             for(t=0;t<cycles;t++)
             {
-                x3=LFSR(l3, arr3, poly3);
-                if(z[t]==x3)
-                    cnt3++;
+                x=LFSR(l, arr, poly);
+                if(z[t]==x)
+                    cnt++;
             }
-            float prob=(float)cnt3/(float)cycles;
+            float prob=(float)cnt/(float)cycles;
             if(prob>0.7 && prob<0.8)
             {
-                for (i= l3 -1; i>=0; i--)
+                for (i= l -1; i>=0; i--)
                 {
-                    deriv_st3[i]=init_st3[i];
-
-                    ofp.write(init_st3[i]+"\t");
+                    deriv_st[i]=init_st[i];
+                    ofp.print(init_st[i]+"\t");
                 }
-                ofp.write(String.format("   %d:\t p(z(t)=x2(t))=%.2f\n", st, prob));
+                ofp.print(String.format("   %d:\t p(z(i)=x2(i))=%.2f\n", st, prob));
 
             }
         }
--- src/LFSR.java
+++ src/LFSR.java
@@ -1,12 +1,11 @@
 import java.io.*;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class LFSR {
-    static int[] poly1 = {1,0,0,0,0,0,1}; // feedback polynomial of degree 7
-    static int[] poly2 = {0,0,0,1,0,0,0,0,1}; // feedback polynomial of degree 9
-    static int[] poly3 = {0,1,0,0,0,0,0,0,0,0,1}; // feedback polynomial of degree 11
-
-
+    static int[] poly1 = {1,0,0,0,0,0,1}; // LFSR1
+    static int[] poly2 = {0,0,0,1,0,0,0,0,1}; // LFSR2
+    static int[] poly3 = {0,1,0,0,0,0,0,0,0,0,1}; // LFSR3
     static int[] arr1, arr2, arr3;
     static int LFSR(int length, int[] arr, int[] poly) {
         int fb = 0;
@@ -19,11 +18,14 @@ public class LFSR {
         return arr[0];
     }
     public static void main(String[] args) throws IOException {
-        BufferedReader ifp = new BufferedReader(new FileReader("src/inp.txt"));
-        BufferedWriter ofp = new BufferedWriter(new FileWriter("key_seq.txt"));
-        BufferedWriter ofp1 = new BufferedWriter(new FileWriter("lfsr_seq1.txt"));
-        BufferedWriter ofp2 = new BufferedWriter(new FileWriter("lfsr_seq2.txt"));
-        BufferedWriter ofp3 = new BufferedWriter(new FileWriter("lfsr_seq3.txt"));
+        Scanner ifp = new Scanner(new File("src/inp.txt"));
+        PrintWriter ofp = new PrintWriter(new FileWriter("key_seq.txt"));
+        PrintWriter ofp1 = new PrintWriter(new FileWriter("lfsr_seq1.txt"));
+        PrintWriter ofp2 = new PrintWriter(new FileWriter("lfsr_seq2.txt"));
+        PrintWriter ofp3 = new PrintWriter(new FileWriter("lfsr_seq3.txt"));
+        System.out.println("Choose 1 for Geffe Generator or choose 2 for other generator:");
+        Scanner function = new Scanner(System.in);
+        String input=function.nextLine();
 
         int no_lfsr, l1, l2, l3, no_clk;
         int[] init_st1, init_st2, init_st3;
@@ -31,106 +33,86 @@ public class LFSR {
         int cnt1 = 0, cnt2 = 0, cnt3 = 0;
 
 
-        /**************OPENING THE DATA FILES*****************/
-
-//        if (ifp == null) {
-//            System.out.println("\n Unable to open input file.\n");
-//            System.exit(0);
-//        }
-
-        /*****************READING THE INPUT FILE******************/
-
-        String line = ifp.readLine();
-        //no_lfsr = Integer.parseInt(line);
-        l1 = Integer.parseInt(ifp.readLine());
+        no_lfsr=ifp.nextInt();
+        l1 = ifp.nextInt();
 
         init_st1 = new int[l1];
-        String [] niza1=ifp.readLine().split(" ");
         arr1 = new int[l1];
         for (i = l1 - 1; i >= 0; i--) {
 
-            init_st1[i] = Integer.parseInt(niza1[i]);
+            init_st1[i] = ifp.nextInt();
             arr1[i] = init_st1[i];
         }
-        l2 = Integer.parseInt(ifp.readLine());
+        l2 = ifp.nextInt();
         init_st2 = new int[l2];
         arr2 = new int[l2];
-        String [] niza2=ifp.readLine().split(" ");
+
         for (i = l2 - 1; i >= 0; i--) {
-            init_st2[i] = Integer.parseInt(niza2[i]);
+            init_st2[i] =ifp.nextInt();
             arr2[i] = init_st2[i];
         }
-        l3 = Integer.parseInt(ifp.readLine());
+        l3 = ifp.nextInt();
         init_st3 = new int[l3];
         arr3 = new int[l3];
-        String [] niza3=ifp.readLine().split(" ");
+
         for (i = l3 - 1; i >= 0; i--) {
-            init_st3[i] = Integer.parseInt(niza3[i]);
+            init_st3[i] = ifp.nextInt();
             arr3[i] = init_st3[i];
         }
-
-        no_clk = Integer.parseInt(ifp.readLine());
-        System.out.printf("\n%d\n",no_clk);
-        //test
-//        int[] test={1, 0, 0, 1, 0, 1, 0};
-//        int[] polytest={1,0,0,0,0,0,1};
-//        //System.out.printf("%d",test.length);
-//        for(k=0;k<no_clk;k++){
-//            x1=LFSR(7,test,polytest);
-//            System.out.printf("%d",x1);
-//        }
-
-//
-       /******************************CLOCKING THE LFSRs*******************/
-
+        no_clk = ifp.nextInt();
         int period1 = 0, period2 = 0, period3 = 0;
         for (k = 0; k < no_clk; k++) {
             x1 = LFSR(l1, arr1, poly1);
-            System.out.printf("%d\t", x1);
-            ofp1.write(String.format("%d",x1));
-
+            ofp1.print(String.format("%d",x1));
             x2 = LFSR(l2, arr2, poly2);
-            ofp2.write(String.format("%d",x2));
-
+            ofp2.print(String.format("%d",x2));
             x3 = LFSR(l3, arr3, poly3);
-            ofp3.write(String.format("%d",x3));
-
-            f=(x1*x2)^(x2*x3)^x3; //non-linear function Geffe generator
-
-            ofp.write(String.format("%d",f));
-
+            ofp3.print(String.format("%d",x3));
+            if(input.equals("1"))
+            {
+                //non-linear function Geffe generator
+                f=(x1*x2)^(x2*x3)^x3;
+            }
+            else {
+                //other non-linear function
+                f=x1^x2^x3;
+            }
+            //generate key stream
+            ofp.print(String.format("%d",f));
             if (f == x1) cnt1++;
             if (f == x2) cnt2++;
             if (f == x3) cnt3++;
-
             for (j = 0; j < l1; j++)
                 if (arr1[j] != init_st1[j])
                     break;
             if (j == l1 && period1 == 0)
                 period1 = k + 1;
-
             for (j = 0; j < l2; j++)
                 if (arr2[j] != init_st2[j])
                     break;
             if (j == l2 && period2 == 0)
                 period2 = k + 1;
-
             for (j = 0; j < l3; j++)
                 if (arr3[j] != init_st3[j])
                     break;
             if (j == l3 && period3 == 0)
                 period3 = k + 1;
-
         }
-        System.out.printf("cnt1 %d \n cnt2 %d \n cnt3 %d",cnt1,cnt2,cnt3);
+        float prob1,prob2,prob3;
+        prob1=(float) cnt1 / (float)no_clk;
+        prob2=(float) cnt2 / (float)no_clk;
+        prob3=(float) cnt3 / (float)no_clk;
+        System.out.println("In this example we are using the following LFSRs:");
+        System.out.println("1) 1+x+x^7 ");
+        System.out.println("2) 1+x^4+x^9");
+        System.out.println("3) 1+x^2+x^11");
+        System.out.printf("\n Probability for occurrence x1(i)=z(i) = %.2f....", prob1);
+        System.out.printf("\n Probability for occurrence x2(i)=z(i)= %.2f....", prob2);
+        System.out.printf("\n Probability for occurrence x3(i)=z(i)= %.2f....", prob3);
 
-        System.out.printf("\n Probability for occurrence x1(t)=z(t) = %.2f....%d", (float) cnt1 / (float)no_clk, cnt1);
-        System.out.printf("\n Probability for occurrence x2(t)=z(t)= %.2f....%d", (float) cnt2 / (float)no_clk, cnt2);
-        System.out.printf("\n  Probability for occurrence x3(t)=z(t)= %.2f....%d", (float) cnt3 / (float) no_clk, cnt3);
-
-        System.out.printf("\n Minimum period of LFSR1= %d\n", period1);
-        System.out.printf("\n Minimum period of LFSR2= %d\n", period2);
-        System.out.printf("\n Minimum period of LFSR3= %d\n", period3);
+        System.out.printf("\n Minimum period of LFSR1= %d", period1);
+        System.out.printf("\n Minimum period of LFSR2= %d", period2);
+        System.out.printf("\n Minimum period of LFSR3= %d", period3);
 
 
         ifp.close();
